71071ea6326daedf707a34fe82bcf839db836726
nikhil <nikhil@mandeeps.com>
2020-10-13 13:04:57 +0530

    add Google ReCaptcha connector

---
+++ DesktopModules/Vanjaro/UXManager/Extensions/Menu/GoogleReCaptcha/Controllers/UIController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vanjaro.Common.Engines.UIEngine;
+
+namespace Vanjaro.UXManager.Extensions.Menu.GoogleReCaptcha.Controllers
+{
+    public class UIController : UIEngineController
+    {
+        public override List<IUIData> GetData(string Identifier, Dictionary<string, string> Parameters)
+        {
+            switch ((Factories.AppFactory.Identifier)Enum.Parse(typeof(Factories.AppFactory.Identifier), Identifier))
+            {
+                case Factories.AppFactory.Identifier.setting_setting:
+                    return SettingController.GetData(PortalSettings.PortalId, UserInfo);
+                default:
+                    break;
+            }
+            return base.GetData(Identifier, Parameters);
+        }
+
+        public override string AccessRoles()
+        {
+            return Factories.AppFactory.GetAccessRoles(UserInfo);
+        }
+        public override string AllowedAccessRoles(string Identifier)
+        {
+            return Factories.AppFactory.GetAllowedRoles(Identifier);
+        }
+    }
+}
