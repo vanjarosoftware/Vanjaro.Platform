79680f1a7016de545e5423836853825a01b1d3bd
Himanshu Sharma <himanshu.sharma@mandeeps.com>
2020-12-08 09:24:33 +0530

    remove unnessary import name space of reflection

--- Portals/_default/Skins/Vanjaro/Base.ascx.cs
+++ Portals/_default/Skins/Vanjaro/Base.ascx.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Web;
 using System.Web.UI;
