aab7b5786ec0f02046d4bbdacdfb1e3a9e0a0057
Manish Kumar <manish.kumar@mandeeps.com>
2020-12-23 11:13:51 +0530

    improved code and msgs

--- DesktopModules/Vanjaro/UXManager/Extensions/Apps/SectionSettings/Views/Settings/Permission.js
+++ DesktopModules/Vanjaro/UXManager/Extensions/Apps/SectionSettings/Views/Settings/Permission.js
@@ -10,6 +10,7 @@
         };
         common.webApi.post('permission/update', 'entityid=' + $scope.ui.data.EntityID.Value + '&entity=' + $scope.ui.data.Entity.Value, data).success(function (Response) {
             if (Response.IsSuccess) {
+                var attrAdded = false;
                 var target = window.parent.document.vj_personalization_target;
                 if (target != undefined) {
                     if (Response.Data <= 0) {
@@ -17,10 +18,12 @@
                         delete attr.perm;
                         target.setAttributes(attr);
                     }
-                    else if (Response.Data > 0 && target.attributes.attributes.perm == undefined)
+                    else if (Response.Data > 0 && target.attributes.attributes.perm == undefined) {
                         target.addAttributes({ perm: Response.Data });
+                        attrAdded = true;
+                    }
                 }
-                $scope.Click_Cancel('update');
+                $scope.Click_Cancel('update', attrAdded);
             }
             else {
                 window.parent.ShowNotification('', Response.Message, 'error');
@@ -28,8 +31,39 @@
         });
     };
 
-    $scope.Click_Cancel = function (type) {
+    $scope.Click_Cancel = function (type, attrAdded) {
+        if (attrAdded)
+            window.parent.ShowNotification('[L:Section]', '[L:PermissionAddedSuccess]', 'warning');
+        else
+            window.parent.ShowNotification('[L:Section]', '[L:PermissionUpdatedSuccess]', 'success');
         window.parent.document.callbacktype = type;
         $(window.parent.document.body).find('[data-dismiss="modal"]').click();
+    };
+
+    $scope.$watch('PermissionsInherit', function (newValue, oldValue) {
+        if (newValue != undefined) {
+            $scope.ShowHideGrid(newValue);
+        }
+    });
+
+    $scope.ShowHideGrid = function (type) {
+        setTimeout(function () {
+            if (type) {
+                $('.addroles').hide();
+                $('.rolesdiv').hide();
+                $('.addusers').hide();
+                $('.usersdiv').hide();
+                $('.empty_msg').hide();
+                $('.permission_grid hr').hide();
+            }
+            else {
+                $('.addroles').show();
+                $('.rolesdiv').show();
+                $('.addusers').show();
+                $('.usersdiv').show();
+                $('.empty_msg').show();
+                $('.permission_grid hr').show();
+            }
+        }, 10);
     };
 });
