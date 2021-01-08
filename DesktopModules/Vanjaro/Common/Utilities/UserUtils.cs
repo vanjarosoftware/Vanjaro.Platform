7b81309a94d249d4f05b7e944c12112b7235d2e1
Narendra Singh <narendra.singh@mandeeps.com>
2021-01-08 14:56:29 +0530

    #499 fixed

--- DesktopModules/Vanjaro/Common/Utilities/UserUtils.cs
+++ DesktopModules/Vanjaro/Common/Utilities/UserUtils.cs
@@ -117,6 +117,11 @@ namespace Vanjaro.Common.Utilities
                 return PhotoURL;
             }
 
+            if (UserID == -1 && string.IsNullOrEmpty(Email))
+            {
+                return VirtualPathUtility.ToAbsolute("~/DesktopModules/Vanjaro/Common/Resources/Images/unknown_avatar.png");
+            }
+
             string Url = GetImageUrl(PortalID, Email);
             if (!string.IsNullOrEmpty(Url))
             {
