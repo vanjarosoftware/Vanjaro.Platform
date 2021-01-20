d880d9c577795edb6a7e5df0e106bb78d8745e3e
ishitagupta94 <ishita.gupta@mandeeps.com>
2021-01-20 18:24:53 +0530

    Revert "updated icon block"
    This reverts commit 432cdc6212beb1968fa3268b92059d48a5833a43.

--- DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/traits.js
+++ DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/traits.js
@@ -648,10 +648,6 @@ export default (editor, config = {}) => {
 
 				if (component.attributes.type == 'carousel-image')
 					component.parent().parent().addAttributes({ href: href });
-				else if (component.attributes.type == 'icon') {
-					component.set('tagName', 'a');
-					component.addAttributes({ href: href });
-				}
 				else
 					component.addAttributes({ href: href });
 			}
