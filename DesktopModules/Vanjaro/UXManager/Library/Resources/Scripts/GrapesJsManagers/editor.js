e435cb8f0143ea00102d9a6f6ccde3b3b8271819
Deepak Verma <deepak.verma@mandeeps.com>
2021-01-28 15:09:31 +0530

    Conflict Resolved

--- DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/GrapesJsManagers/editor.js
+++ DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/GrapesJsManagers/editor.js
@@ -1540,12 +1540,6 @@ $(document).ready(function () {
 
                                 if (typeof model != 'undefined' && typeof model.modelToDrop != 'undefined') {
 
-                                    if (typeof model.modelToDrop.parent != 'undefined' && model.modelToDrop && model.modelToDrop.attributes.type == "videobox") {
-                                        var videoID = model.modelToDrop.attributes.src;
-                                        model.modelToDrop.findType('video')[0].attributes.src = videoID;
-                                        $(model.modelToDrop.findType('video')[0].getEl())[0].children[0].attributes[0].nodeValue = videoID;                                                       
-                                    }
-
 									if (typeof model.modelToDrop.parent != 'undefined' && model.modelToDrop.parent() && model.modelToDrop.parent().attributes.type == "column")
 										$(model.modelToDrop.parent().getEl()).removeAttr("data-empty");
                                     if (typeof model.modelToDrop.attributes != 'undefined' && model.modelToDrop.attributes.type == "videobox") {
