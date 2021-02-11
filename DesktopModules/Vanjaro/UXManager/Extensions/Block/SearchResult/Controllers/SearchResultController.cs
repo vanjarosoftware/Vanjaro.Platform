76574b148f14a4538077ebd1f8044c7a03582637
Himanshu Sharma <himanshu.sharma@mandeeps.com>
2021-02-11 16:53:39 +0530

    Style implemented in Search (input/result)

--- DesktopModules/Vanjaro/UXManager/Extensions/Block/SearchResult/Controllers/SearchResultController.cs
+++ DesktopModules/Vanjaro/UXManager/Extensions/Block/SearchResult/Controllers/SearchResultController.cs
@@ -3,10 +3,14 @@ using DotNetNuke.Entities.Users;
 using DotNetNuke.Web.Api;
 using DotNetNuke.Web.Api.Internal;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Http;
 using Vanjaro.Common.ASPNET.WebAPI;
 using Vanjaro.Common.Engines.UIEngine;
+using Vanjaro.Core.Components;
+using static Vanjaro.Core.Managers;
 
 namespace Vanjaro.UXManager.Extensions.Block.SearchResult.Controllers
 {
@@ -16,13 +20,15 @@ namespace Vanjaro.UXManager.Extensions.Block.SearchResult.Controllers
     {
         internal static List<IUIData> GetData(UserInfo userInfo, string identifier, Dictionary<string, string> parameters, PortalSettings portalSettings)
         {
+            List<StringText> templates = GetTemplates();
             Dictionary<string, IUIData> Settings = new Dictionary<string, IUIData>
             {
                 { "Global", new UIData { Name = "Global", Value = "true" } },
                 { "LinkTarget", new UIData { Name = "LinkTarget", Value = "true" } },
                 { "EnableWildSearch", new UIData { Name = "EnableWildSearch", Value = "true" } },
                 { "GlobalConfigs", new UIData { Name = "GlobalConfigs", Options = Core.Managers.BlockManager.GetGlobalConfigs(portalSettings, "search result") } },
-                { "IsAdmin", new UIData { Name = "IsAdmin", Value = userInfo.IsInRole("Administrators").ToString().ToLower() } }
+                { "IsAdmin", new UIData { Name = "IsAdmin", Value = userInfo.IsInRole("Administrators").ToString().ToLower() } },
+                { "Template", new UIData { Name = "Template", Options = templates, OptionsText = "Text", OptionsValue = "Value" } }
             };
             return Settings.Values.ToList();
         }
@@ -34,6 +40,31 @@ namespace Vanjaro.UXManager.Extensions.Block.SearchResult.Controllers
         {
             Core.Managers.BlockManager.UpdateDesignElement(PortalSettings, Attributes);
         }
+
+
+        private static List<StringText> GetTemplates()
+        {
+            string TemplatesPath = HttpContext.Current.Server.MapPath("~/Portals/_default/vThemes/" + ThemeManager.CurrentTheme.Name + "/blocks/search result/Templates");
+            List<StringText> Templates = new List<StringText>();
+            if (Directory.Exists(TemplatesPath))
+            {
+                foreach (string file in Directory.GetFiles(TemplatesPath))
+                {
+                    string FileName = Path.GetFileName(file);
+                    if (!string.IsNullOrEmpty(FileName))
+                    {
+                        if (FileName.EndsWith(".cshtml"))
+                        {
+                            FileName = FileName.Replace(".cshtml", "");
+                            Templates.Add(new StringText() { Value = FileName.ToString(), Text = FileName.ToString() });
+                        }
+                    }
+                }
+            }
+
+            return Templates;
+        }
+
         public override string AccessRoles()
         {
             return Factories.AppFactory.GetAccessRoles(UserInfo);
