70da8f5594902f60565275ea0247d76f40c6bf0e
Gagandeep Singh <gagan@mandeeps.com>
2021-04-16 16:53:37 +0530

    Fixed font size trait

--- DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/list.js
+++ DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/list.js
@@ -171,10 +171,14 @@ export default (editor, config = {}) => {
 					name: "fontsize",
 					type: "custom_range",
 					cssproperties: [{ name: "font-size" }],
-					units: [
-						{ name: 'px', min: 10, max: 100, step: 1, value: 16 },
-						{ name: 'vw', min: 0.5, max: 10, step: 0.1, value: 1 },
-					],
+                        units: [
+                            { name: 'px', min: 10, max: 100, step: 1, value: 16 },
+                            { name: '%', min: 10, max: 100, step: 1, value: 100 },
+                            { name: 'em', min: 0.5, max: 10, step: 0.1, value: 1 },
+                            { name: 'rem', min: 0.5, max: 10, step: 0.1, value: 1 },
+                            { name: 'vw', min: 0.5, max: 10, step: 0.1, value: 1 },
+                            { name: 'vh', min: 0.5, max: 10, step: 0.1, value: 1.5 },
+                        ],
 					unit: "px",
 					changeProp: 1,
 				}, {
