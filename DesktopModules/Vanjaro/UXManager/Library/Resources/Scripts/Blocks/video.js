79c2dbf935a535ab60a7c11a09355500d9174d9b
swanandpachode94 <59220898+swanand93@users.noreply.github.com>
2021-07-20 13:51:30 +0530

    playsinline attribute added in video tag

--- DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/video.js
+++ DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/video.js
@@ -9,7 +9,7 @@
 			attributes: { class: 'fa fa-youtube-play' },
 			content: `
 				<div class='video-box embed-container'>
-					<video controls src='`+ VjDefaultPath + `Flower.mp4'></video>
+					<video controls src='`+ VjDefaultPath + `Flower.mp4' playsinline></video>
 				</div>	
 			`,
 			activate: 1
@@ -194,7 +194,7 @@
 					this.set({ 'src': 'https://www.youtube.com/embed/' });
 				}
 				else {
-					this.components().models[0].replaceWith('<video src="' + VjDefaultPath + 'Flower.mp4"></video>');
+					this.components().models[0].replaceWith('<video src="' + VjDefaultPath + 'Flower.mp4" autoplay muted playsinline></video>');
 					this.loadTraits(this.getSourceTraits());
 					this.set({ 'src': '' + VjDefaultPath + 'Flower.mp4' });
 					this.getTrait('controls').setTargetValue('controlstrue');
