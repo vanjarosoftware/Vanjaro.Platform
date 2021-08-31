fff6e0cffdee97bf59037c84655c29f83364a848
Manish Kumar <manish.kumar@mandeeps.com>
2021-08-31 11:44:44 +0530

    update build 1.1.1

--- DesktopModules/Vanjaro/URLManager/Properties/AssemblyInfo.cs
+++ DesktopModules/Vanjaro/URLManager/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vanjaro.URL")]
+[assembly: AssemblyTitle("ASPNET")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vanjaro.URL")]
+[assembly: AssemblyProduct("ASPNET")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,15 +19,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f3ae5586-6e24-4687-bf5d-a2bce720a137")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.9.0")]
-[assembly: AssemblyFileVersion("1.3.9.0")]
