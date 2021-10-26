463efdadd0b49edf8dd1abe33ef5eab15b00dc62
ishitagupta94 <ishita.gupta@mandeeps.com>
2021-10-26 13:18:19 +0530

    improved trait

--- DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/traits.js
+++ DesktopModules/Vanjaro/UXManager/Library/Resources/Scripts/Blocks/traits.js
@@ -468,25 +468,10 @@ export default (editor, config = {}) => {
 			return el;
 		},
 		eventCapture: ['input'],
-		debounce: function (func, wait, immediate) {
-			var timeout;
-			return function () {
-				var context = this, args = arguments;
-				var later = function () {
-					timeout = null;
-					if (!immediate) func.apply(context, args);
-				};
-				var callNow = immediate && !timeout;
-				clearTimeout(timeout);
-				timeout = setTimeout(later, wait);
-				if (callNow) func.apply(context, args);
-			};
-		},
 		onEvent({ elInput, component, event }) {
 
 			var caption = elInput.querySelector('textarea').value;
-			component.addAttributes({ alt: caption });
-			component.getTrait('caption').setTargetValue(caption);
+			component.getTrait(event.target.name).setTargetValue(caption);
 		}
 	});
 
@@ -772,13 +757,16 @@ export default (editor, config = {}) => {
 			}
 			else if (event.target.name == "target") {
 
-				if (event.target.id == "yes")
+				if (event.target.id == "yes") {
 					model.addAttributes({ 'target': '_blank', 'rel': 'noopener' });
+					model.set({ 'target': 'yes' });
+				}
 				else {
 					const attr = model.getAttributes();
 					delete attr.rel;
 					delete attr.target;
 					model.setAttributes(attr);
+					model.set({ 'target': 'no' });
 				}
 			}
 			else
