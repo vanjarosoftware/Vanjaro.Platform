bc8657784f5c659944aa787352423223b0fa97f6
Himanshu Sharma <himanshu.sharma@mandeeps.com>
2022-01-28 15:02:00 +0530

    Not redirect set page of Terms & Privacy on Sign Up block

--- DesktopModules/Vanjaro/UXManager/Extensions/Block/Register/Entities/Register.cs
+++ DesktopModules/Vanjaro/UXManager/Extensions/Block/Register/Entities/Register.cs
@@ -6,12 +6,41 @@ namespace Vanjaro.UXManager.Extensions.Block.Register.Entities
 {
     public class Register
     {
+        private PortalSettings PortalSettings = null;
+        public Register()
+        {
+            PortalSettings = (PortalController.Instance.GetCurrentSettings() as PortalSettings);
+        }
         public bool ShowLabel { get; set; }
         public bool TermsPrivacy { get; set; }
         public string ButtonAlign { get; set; }
         public string LoginURL => Managers.LoginLinkManager.LoginURL("", false);
-        public string TermsURL => ServiceProvider.NavigationManager.NavigateURL("Terms");
-        public string PrivacyURL => ServiceProvider.NavigationManager.NavigateURL("Privacy");
-        public int RegistrationMode => (PortalController.Instance.GetCurrentSettings() as PortalSettings).UserRegistration;
+        public string TermsURL
+        {
+            get
+            {
+                if (PortalSettings.TermsTabId > 0)
+                    return Globals.NavigateURL(PortalSettings.TermsTabId);
+                else
+                    return ServiceProvider.NavigationManager.NavigateURL("Terms");
+            }
+        }
+        public string PrivacyURL
+        {
+            get
+            {
+                if (PortalSettings.TermsTabId > 0)
+                    return Globals.NavigateURL(PortalSettings.PrivacyTabId);
+                else
+                    return ServiceProvider.NavigationManager.NavigateURL("Privacy");
+            }
+        }
+        public int RegistrationMode
+        {
+            get
+            {
+                return PortalSettings.UserRegistration;
+            }
+        }
     }
 }
