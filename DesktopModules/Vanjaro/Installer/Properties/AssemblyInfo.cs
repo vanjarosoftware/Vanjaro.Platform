15e3cd1cfaa280db85ed78f74cb2b03cee8d14b3
Manish Kumar <manish.kumar@mandeeps.com>
2022-02-17 11:46:00 +0530

    Fixed Installer

--- DesktopModules/Vanjaro/Installer/Properties/AssemblyInfo.cs
+++ DesktopModules/Vanjaro/Installer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("94be8adc-cb86-477a-a734-0bb7a70dba9f")]
+[assembly: Guid("ac4690f0-7e20-4270-a94c-392e246b00e2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
